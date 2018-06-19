4dbc1db9d1ee63c124d57fa5a4aea01f0616e832
Ganesh K <ganesh3597@gmail.com>
2018-06-19 10:23:00 +0530

    Skip recognize on osx

--- tests/test_recognizer.py
+++ tests/test_recognizer.py
@@ -1,12 +1,14 @@
 import pytest
 import pkg_resources
 import os
+import sys
 
 from titanium_rhythm.setup_api import Setup
 from titanium_rhythm.identify import Identify
 
 SONG_FILE = pkg_resources.resource_filename('titanium_rhythm', os.path.join('songs', 'unknown.mp3'))
 
+@pytest.mark.skipif(sys.platform == 'darwin', reason="does not run on osx yet")
 def test_identify():
 	Setup.set_key('PMaILC8xG3')
 	i = Identify(SONG_FILE)
