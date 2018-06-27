c1180f2ff30852585b80953acf74fdd4789c3e45
Ganesh K <ganesh3597@gmail.com>
2018-06-27 09:01:28 +0530

    Bug Fixes

--- tests/test_annotate.py
+++ tests/test_annotate.py
@@ -11,10 +11,11 @@ import sys
 
 from titanium_rhythm.annotate import annotate_file
 
-SONG_FILE = pkg_resources.resource_filename('titanium_rhythm', os.path.join('songs', '23.mp3'))
+BEFORE_SONG_FILE = pkg_resources.resource_filename('titanium_rhythm', os.path.join('songs', '23.mp3'))
+AFTER_SONG_FILE = pkg_resources.resource_filename('titanium_rhythm', os.path.join('songs', 'Counting Stars.mp3'))
 
 @pytest.mark.skipif(sys.platform == 'darwin', reason="does not run on osx yet")
 def test_annotate():
-	annotate_file('PMaILC8xG3', SONG_FILE)
-	
+	annotate_file('PMaILC8xG3', BEFORE_SONG_FILE)
+	os.rename(AFTER_SONG_FILE, BEFORE_SONG_FILE)
 
