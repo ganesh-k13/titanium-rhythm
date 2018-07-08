9d5e2b494c14b61ab663ee29a1eea7de7890b973
ganesh <ganesh3597@gmail.com>
2018-07-08 10:32:39 +0530

    Discogs API key

--- titanium_rhythm/setup_api.py
+++ titanium_rhythm/setup_api.py
@@ -5,13 +5,16 @@ import os
 DATA_PATH = pkg_resources.resource_filename('titanium_rhythm', 'info/')
 JSON_FILE = pkg_resources.resource_filename('titanium_rhythm', os.path.join('info', 'info.json'))
 
+info = dict()
+info['api_key'] = ''
+info['discogs_key'] = ''
+
 class Setup():
     def __init__(self):
         pass
     
     @staticmethod
     def set_key(key):
-        info = dict()
         info['api_key'] = key
         with open(JSON_FILE, 'w') as f:
             json.dump(info, f, indent = 4)
@@ -21,5 +24,18 @@ class Setup():
         with open(JSON_FILE, 'r') as f:
             info = json.load(f)
         
+        return info
+
+    @staticmethod
+    def set_discogs_key(key):
+        info['discogs_key'] = key
+        with open(JSON_FILE, 'w') as f:
+            json.dump(info, f, indent = 4)
+
+    @staticmethod
+    def get_disogs_key():
+        with open(JSON_FILE, 'r') as f:
+            info = json.load(f)
+
         return info
             
